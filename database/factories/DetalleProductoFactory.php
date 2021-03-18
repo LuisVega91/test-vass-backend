52f8daf52185018428ef0f86feac39db75014784
LuisVega91 <luisevegam.91@gmail.com>
2021-03-18 05:56:46 -0500

    ajuste semillas de kardex

--- database/factories/DetalleProductoFactory.php
+++ database/factories/DetalleProductoFactory.php
@@ -6,17 +6,16 @@ use App\DetalleProducto;
 use Faker\Generator as Faker;
 
 $factory->define(DetalleProducto::class, function (Faker $faker) {
-    return [
-        'descripcion'=>  $faker->sentence,
 
-        'valor_unitario'=>  $faker->numberBetween(10, 20),
-        'entrada_cantidad'=>  $faker->numberBetween(10, 20),
-        'entrada_valor'=>  $faker->numberBetween(10, 20),
-        'salida_cantidad'=>  $faker->numberBetween(10, 20),
-        'salida_valor'=>  $faker->numberBetween(10, 20),
-        'saldo_cantidad'=>  $faker->numberBetween(10, 20),
-        'saldo_valor'=>  $faker->numberBetween(10, 20),
-        
-        'id_producto'=>  $faker->numberBetween(1, 10),
+    return [
+        'descripcion' =>  $faker->sentence,
+        'valor_unitario' => $faker->numberBetween(10, 20),
+        'entrada_cantidad' => $faker->numberBetween(10, 20),
+        'entrada_valor' =>  $faker->numberBetween(10, 20),
+        'salida_cantidad' => 0,
+        'salida_valor' => 0,
+        'saldo_cantidad' => $faker->numberBetween(40, 80),
+        'saldo_valor' => $faker->numberBetween(200, 500),
+        'id_producto' =>  $faker->numberBetween(1, 10),
     ];
 });
